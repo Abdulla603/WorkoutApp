7453684d494420e67f6a434f2db4b2fc879e2499
ethanstanley <49345412+ethanstanley@users.noreply.github.com>
2020-12-08 00:07:12 -0500

    Add files via upload

---
+++ WorkoutApp-main/IOSFinalProject/WorkoutCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  WorkoutCollectionViewCell.swift
+//  IOSFinalProject
+//
+//  Created by Ethan Stanley on 12/7/20.
+//
+
+import UIKit
+
+class WorkoutCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ WorkoutApp-main/IOSFinalProject/workout.swift
@@ -0,0 +1,15 @@
+//
+//  Workout.swift
+//  IOSFinalProject
+//
+//  Created by Ethan Stanley on 12/7/20.
+//
+
+import Foundation
+
+struct Workout: Equatable {
+    var imageName: String
+    var name: String
+    var muscleTarget: String
+    var exerciseOworkout: Boolean
+}
